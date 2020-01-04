087b2a3c83ddc7642e986c96181b8d4d55289048
dexter090907 <59513514+dexter090907@users.noreply.github.com>
2020-01-04 17:18:08 -0600

    Update dexter1999.sh

--- dexter1999.sh
+++ dexter1999.sh
@@ -16,3 +16,29 @@ for i in $lista
     do
     echo "$i"
 done 
+sleep 4
+echo ""
+sleep 2
+echo -e "\e[1;30m"
+echo "      /| ________________"
+echo " 0|===|* >________________>"
+echo "      \|"
+echo ""
+echo -e "\e[35mɠεɳεɾα∂σɾ ∂ε ραყℓσα∂ ραɾα ɦƭƭρ เɳʝεcƭσɾ"
+echo ""
+sleep 2
+echo -e "\e[1;36mcɾεα∂σɾ ∂εℓ รcɾเρƭ ∂εאƭεɾ1999 \e[0m"
+echo -e "\e[35m"
+echo "ejemplos de host"
+echo -e "\e[1;36m"
+echo -e "\e[1;36m"
+lista="facebook.com google.com instagram.com"
+for i in $lista
+    do
+    echo "$i"
+done 
+echo "" 
+echo -ne "\e[1;36mINGRESA TU HOST: \e[1;33m"
+read host 
+echo -e "\e[1;33m"
+echo "GENERANDO PAYLOADS..."
