01c43600489c590b63da321608f6810a3e3e195c
dexter090907 <59513514+dexter090907@users.noreply.github.com>
2020-01-04 17:04:33 -0600

    Update dexter1999.sh

--- dexter1999.sh
+++ dexter1999.sh
@@ -1,4 +1,18 @@
+#!bin/bash       
+#bi:dexter1999
+echo -e "\e[1;33m"
+echo "★☣⋙∂ε∂เcα∂σ α ɱเ ɠɾµρσ ∂ε ωɦαƭรαρρ⋘☯★"
+echo ""
+sleep 1
+echo -e "\e[36m B¡ทℯℛℴ₷ ðℯ ☪ℴℛα☡óท ƴ ℳα₷ \e[0m"
 
-
-#!bin/bash
-echo "dexter1999" 
+echo -e "\e[1;36m"
+sleep 2
+echo "∂ε∂เcα∂σ α ℓσร α∂ɱเɳเรƭɾα∂σɾεร ∂ε ɱเร ɠɾµρσร ɠɾαcเαร ρεɾɾร"
+echo ""
+sleep 3
+lista=">>∂αɾωเɳ >>ҡαɾɱαɳ >>ɳεɾყ >>ωαɾɳเɳɠ >>ƒεℓเρε >>εℓ ℓเc >>uᴉʌǝꞀ"
+for i in $lista
+    do
+    echo "$i"
+done 
